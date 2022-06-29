cf6a569d987b5f160c774a9309415ac46bcbc6b2
danilopetrovic88 <vivify@VivifyIdeass-MacBook-Pro-3.local>
2022-06-29 19:06:49 +0200

    Zadatak 1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
+import { Route } from 'react-router-dom';
+import AppPosts from './pages/AppPosts';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <nav className="navbar navbar-light bg-light">
+          <Link to={'/posts'} className="navbar-brand">
+            Posts
+          </Link>
+        </nav>
+        <Switch>
+          <Route>
+            <AppPosts />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/AppPosts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AppPosts = () => {
+  return (
+    <div>AppPosts Component</div>
+  )
+}
+
+export default AppPosts
