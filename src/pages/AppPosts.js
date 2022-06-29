931a5846ef260ff01116b95b0959afb950deeba9
danilopetrovic88 <vivify@VivifyIdeass-MacBook-Pro-3.local>
2022-06-29 19:18:52 +0200

    zadatak 2

--- src/pages/AppPosts.js
+++ src/pages/AppPosts.js
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import PostService from '../service/PostService';
 
 const AppPosts = () => {
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+          const data = await PostService.getAll();
+    
+          setPosts(data);
+        };
+        fetchPosts();
+      }, []);
+
   return (
-    <div>AppPosts Component</div>
+    <div className='container'>
+        <div className='row'>
+            <div className='col-10 offset-1'>
+                { posts.map((post) => (
+                    <div className='card'>
+                        <div className='card-header'>
+                            {post.title}
+                        </div>
+                        <div className='card-body'>
+                            {post.text}
+                        </div>
+                    </div>
+                )) }
+            </div>
+        </div>
+    </div>
   )
 }
 
---
+++ src/service/PostService.js
@@ -0,0 +1,24 @@
+import axios from "axios"
+
+class PostService {
+    constructor() {
+        this.httpClient = axios.create({
+          baseURL: 'http://localhost:3000/api',
+        });
+      }
+
+      async getAll() {
+        try {
+          const { data } = await this.httpClient.get('posts');
+    
+          return data;
+        } catch (error) {
+          console.log(error);
+        }
+    
+        return [];
+      }
+
+}
+
+export default new PostService()
